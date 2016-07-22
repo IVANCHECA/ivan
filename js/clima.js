2525a702000e25497f718bb37211baea2cc1fe21
ivan checa <ivanchecamora@hotmail.com>
2016-07-22 18:35:11 -0500

    temperatura

--- js/clima.js
+++ js/clima.js
@@ -3,7 +3,20 @@ function loadWeather(id, city) {
     if(prevId!=null){
          $("#" +prevId).toggleClass("selected");
     }
-   
+        $("#city").html(city);
        $("#" +id).toggleClass("selected");
-       prevId=id;
+       prevId=id;       
+       var url= "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%2C%20co%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+       $.get(url, function(data, status){
+           console.log(data);
+           var channel = data.query.results.channel;
+           var atmosphere =channel.atmosphere;
+           var condition  = channel.item.condition;
+                         
+           $("#description").html(condition.text);
+           $("#temp").html(condition.temp);
+           $("#hum").html(atmosphere.humidity);
+           $("#pres").html(atmosphere.pressure);
+
+       });
 }
